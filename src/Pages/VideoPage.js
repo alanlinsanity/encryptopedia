32124b8542b2a3e5c9c42d349f32eebbf2797724
alanlinsanity <alan.linyh@gmail.com>
2022-03-09 19:18:42 +0800

    added videos

--- src/Pages/VideoPage.js
+++ src/Pages/VideoPage.js
@@ -1,12 +1,56 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 
 const VideoPage = () => {
+  const [videos, setVideos] = useState([]);
+  const [search, setSearch] = useState("cryptocurrency");
+  const [render, setRender] = useState("")
+
+
+  const allVideos = () =>
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${search}&key=AIzaSyD7WvZHOahX4B5EXossb1vgV-7s1hGLR1g`;
+
+  //fetching trending coins 
+  const fetchAllVideos = async () => {
+    const { data } = await axios.get(allVideos())
+
+    setVideos(data.items);
+  };
+
+  useEffect(() => {
+    fetchAllVideos();
+  }, [render]);
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    console.log(`The search you entered was: ${search}`)
+    setRender(`${search}`)
+  }
+
   return (
     <div>
-        <h1>Latest Videos on Crypto</h1>
-    </div>
-    
+    <div className="videoPageTitle"> "{render.toUpperCase()}" Videos </div>
+    <form onSubmit={formSubmit}>
+      <label >Search a cryptocurrency:<br/>
+        <input
+          type="text" 
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </label>
+    </form>
+
+      <div className="youtubeVideos">
+      {videos.map((video, index) => (
+            <iframe className="youtubeSingleVideo" key={index} width="420" height="315"
+            src={`https://www.youtube.com/embed/${video.id.videoId}`}>
+            </iframe>
+      ))}
+      </div>
+      </div>
   )
 }
 
